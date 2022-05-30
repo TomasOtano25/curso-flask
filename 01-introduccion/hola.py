659367755fb9b2d650aee773cae32267724fef44
tomasotano25 <tomasotano25@gmail.com>
2022-05-30 16:55:51 -0400

    7 - recorrer listas con jinja

--- 01-introduccion/hola.py
+++ 01-introduccion/hola.py
@@ -6,7 +6,25 @@ app = Flask(__name__)
 @app.route('/')
 def hola():
     mensaje = 'Hola Mundo con Flask'
+
     return render_template('index.html', msm=mensaje)
+
+
+@app.route('/blogs')
+def blog():
+    b1 = Blog('Que es Python?', 'Descripcion de Blog 1')
+
+    b2 = Blog('Que es Flask?', 'Descripcion de Blog 2')
+
+    blogs = [b1, b2]
+
+    return render_template('blog.html', blogs=blogs)
+
+
+class Blog:
+    def __init__(self, titulo, desc):
+        self.titulo = titulo
+        self.desc = desc
 
 
 # python .\hola.py
